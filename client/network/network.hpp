fc9115f49a2f0d9021902272f7b4c1c566c77e80
Paul-Antoine SALMON <paul-antoine.salmon@epitech.eu>
2025-04-02 13:24:14 +0200

    [ADD](Client): Implement Network class for Jetpack client with connection handling and protocol methods

--- client/network/network.hpp
+++ client/network/network.hpp
@@ -1,9 +1,72 @@
+// Copyright 2025 paul-antoine.salmon@epitech.eu
 /*
 ** EPITECH PROJECT, 2025
 ** B-NWP-400-NAN-4-1-jetpack-santiago.pidcova
 ** File description:
-** network
+** Network class for Jetpack client
 */
 
-#pragma once
+#ifndef CLIENT_NETWORK_NETWORK_HPP_
+#define CLIENT_NETWORK_NETWORK_HPP_
+
+#include <string>
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "../protocol.hpp"
+#include "../gamestate.hpp"
+
+namespace jetpack {
+namespace network {
+
+class Network {
+ public:
+  Network(const std::string& host, int port, bool debugMode,
+          GameState* gameState);
+  ~Network();
+
+  bool connect();
+  void disconnect();
+  void run();
+  void stop();
+
+ private:
+  // Connection info
+  std::string host_;
+  int port_;
+  bool debugMode_;
+  int socket_;
+  std::atomic<bool> running_;
+  GameState* gameState_;
+
+  // Thread
+  std::thread networkThread_;
+
+  // Network methods
+  bool sendPacket(protocol::PacketType type, const std::vector<uint8_t>& payload);
+  bool receivePacket(protocol::PacketHeader* header, std::vector<uint8_t>* payload);
+
+  // Protocol handlers
+  void handleServerWelcome(const std::vector<uint8_t>& payload);
+  void handleMapChunk(const std::vector<uint8_t>& payload);
+  void handleGameStart(const std::vector<uint8_t>& payload);
+  void handleGameState(const std::vector<uint8_t>& payload);
+  void handleGameEnd(const std::vector<uint8_t>& payload);
+  void handleDebugInfo(const std::vector<uint8_t>& payload);
+
+  // Input sending
+  void sendPlayerInput();
+
+  // Debug printing
+  void debugPrint(const std::string& message);
+};
+
+}  // namespace network
+}  // namespace jetpack
+
+#endif  // CLIENT_NETWORK_NETWORK_HPP_
 
