e0e2ebe854b8897be4082463ed9c697c0d8d77ff
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-04-14 12:57:08 +0200

    [REFACTOR](Server): Extract header writing logic into a separate function for better code organization

--- server/send_messages_to_clients.c
+++ server/send_messages_to_clients.c
@@ -7,16 +7,21 @@
 
 #include "includes/server.h"
 
+void write_header(uint8_t *buf, uint8_t type, uint16_t total_len)
+{
+    buf[0] = MAGIC_BYTE;
+    buf[1] = type;
+    buf[2] = (total_len >> 8) & 0xFF;
+    buf[3] = total_len & 0xFF;
+}
+
 void send_welcome(int client_fd, uint8_t assigned_id)
 {
     uint8_t buffer[4 + 2];
     uint16_t length = htons(4 + 2);
     ssize_t bytes_sent;
 
-    buffer[0] = MAGIC_BYTE;
-    buffer[1] = SERVER_WELCOME;
-    buffer[2] = (length >> 8) & 0xFF;
-    buffer[3] = length & 0xFF;
+    write_header(buffer, SERVER_WELCOME, length);
     buffer[4] = 1;
     buffer[5] = assigned_id;
     bytes_sent = send(client_fd, buffer, sizeof(buffer), 0);
