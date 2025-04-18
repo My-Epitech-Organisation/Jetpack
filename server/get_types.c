cef98faefad70ba5fa8e8afeca974f43adc2e1cd
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-04-18 12:59:14 +0200

    [STYLE](server): Refactor get_type_string functions to use switch statements for improved readability

--- server/get_types.c
+++ server/get_types.c
@@ -9,28 +9,32 @@
 
 char *get_type_string_next(uint8_t type)
 {
-    if (type == GAME_INPUT)
-        return "GAME_INPUT";
-    if (type == GAME_STATE)
-        return "GAME_STATE";
-    if (type == GAME_END)
-        return "GAME_END";
-    if (type == CLIENT_DISCONNECT)
-        return "CLIENT_DISCONNECT";
-    if (type == DEBUG_INFO)
-        return "DEBUG_INFO";
+    switch (type) {
+        case GAME_INPUT:
+            return "GAME_INPUT";
+        case GAME_STATE:
+            return "GAME_STATE";
+        case GAME_END:
+            return "GAME_END";
+        case CLIENT_DISCONNECT:
+            return "CLIENT_DISCONNECT";
+        case DEBUG_INFO:
+            return "DEBUG_INFO";
+    }
     return "UNKNOWN_TYPE";
 }
 
 char *get_type_string_prev(uint8_t type)
 {
-    if (type == CLIENT_CONNECT)
-        return "CLIENT_CONNECT";
-    if (type == SERVER_WELCOME)
-        return "SERVER_WELCOME";
-    if (type == MAP_CHUNK)
-        return "MAP_CHUNK";
-    if (type == GAME_START)
-        return "GAME_START";
+    switch (type) {
+        case CLIENT_CONNECT:
+            return "CLIENT_CONNECT";
+        case SERVER_WELCOME:
+            return "SERVER_WELCOME";
+        case MAP_CHUNK:
+            return "MAP_CHUNK";
+        case GAME_START:
+            return "GAME_START";
+    }
     return get_type_string_next(type);
 }
