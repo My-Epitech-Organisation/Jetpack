f53889e9a9cf30627ff3a3302ea9e0e58450df67
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-04-20 01:29:59 +0200

    [REFINE](read_client): Update handle_message to use payload directly and improve error handling in send_messages_to_clients

--- server/send_messages_to_clients.c
+++ server/send_messages_to_clients.c
@@ -16,7 +16,7 @@ void send_welcome(server_t *server, int client_fd, uint8_t assigned_id)
     buffer[4] = 1;
     buffer[5] = assigned_id;
     if (!send_with_write(client_fd, buffer, sizeof(buffer)))
-        handle_error("send_with_write SERVER_WELCOME");
+        handle_error("send_with_write SERVER_WELCOME", server);
     print_debug_info_package_sent(server, get_type_string_prev(buffer[1]),
         buffer, sizeof(buffer));
 }
@@ -31,13 +31,13 @@ void send_map_chunk(server_t *server, int client_fd, uint8_t col_index)
     total_size = 4 + (4 + server->map_rows);
     buffer = malloc(total_size);
     if (!buffer)
-        handle_error("malloc");
+        handle_error("malloc", server);
     write_header(buffer, MAP_CHUNK, 4 + 4 + server->map_rows);
     write_map_payload(buffer, col_index, (uint16_t)server->map_cols);
     for (size_t row = 0; row < server->map_rows; row++)
         buffer[8 + row] = server->map[row][col_index];
     if (!send_with_write(client_fd, buffer, total_size))
-        handle_error("send_with_write MAP_CHUNK");
+        handle_error("send_with_write MAP_CHUNK", server);
     print_debug_info_package_sent(server, get_type_string_prev(buffer[1]),
         buffer, total_size);
     free(buffer);
@@ -47,4 +47,21 @@ void send_map(server_t *server, int client_fd)
 {
     for (uint8_t col_index = 0; col_index < server->map_cols; col_index++)
         send_map_chunk(server, client_fd, col_index);
+}
+
+void send_disconnect(server_t *server)
+{
+    uint8_t buffer[4 + 1];
+    uint16_t length = 4 + 1;
+
+    write_header(buffer, CLIENT_DISCONNECT, length);
+    buffer[4] = 0;
+    for (int i = 0; i < server->client_count; i++) {
+        if (server->client[i] == NULL)
+            continue;
+        if (!send_with_write(server->client[i]->fd, buffer, sizeof(buffer)))
+            handle_error("send_with_write CLIENT_DISCONNECT", server);
+        print_debug_info_package_sent(server,
+            get_type_string_prev(buffer[1]), buffer, sizeof(buffer));
+    }
 }
