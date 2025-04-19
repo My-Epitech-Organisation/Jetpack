17baa1e8f116660683902ee7b93875e411ae32da
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-04-20 01:27:34 +0200

    [REFINE](error_handling): Enhance error handling functions to include server context for better resource management

--- server/load_map.c
+++ server/load_map.c
@@ -18,9 +18,9 @@ static void close_and_free(FILE *file, char *line)
         free(line);
 }
 
-void get_map_size(const char *path, size_t *rows, size_t *cols)
+void get_map_size(server_t *server, size_t *rows, size_t *cols)
 {
-    FILE *file = fopen(path, "r");
+    FILE *file = fopen(server->map_path, "r");
     size_t row_count = 0;
     size_t col_count = 0;
     char *line = NULL;
@@ -28,7 +28,7 @@ void get_map_size(const char *path, size_t *rows, size_t *cols)
     ssize_t read;
 
     if (!file)
-        handle_error("fopen");
+        handle_error("fopen", server);
     while (1) {
         read = getline(&line, &len, file);
         if (read == -1)
@@ -42,30 +42,34 @@ void get_map_size(const char *path, size_t *rows, size_t *cols)
     *cols = col_count;
 }
 
-char **allocate_map(size_t rows, size_t cols)
+char **allocate_map(server_t *server)
 {
+    size_t rows = server->map_rows;
+    size_t cols = server->map_cols;
     char **map = malloc(rows * sizeof(char *));
 
     if (!map)
-        handle_error("malloc");
+        handle_error("malloc", server);
     for (size_t i = 0; i < rows; i++) {
         map[i] = malloc(sizeof(char) * (cols + 1));
         if (!map[i])
-            handle_error("malloc");
+            handle_error("malloc", server);
     }
     return map;
 }
 
-void fill_map(const char *path, char **map, size_t rows, size_t cols)
+void fill_map(const char *path, char **map, server_t *server)
 {
     FILE *file = fopen(path, "r");
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
     size_t i = 0;
+    size_t rows = server->map_rows;
+    size_t cols = server->map_cols;
 
     if (!file)
-        handle_error("fopen");
+        handle_error("fopen", server);
     while (i < rows) {
         read = getline(&line, &len, file);
         if (read == -1)
@@ -80,8 +84,7 @@ void fill_map(const char *path, char **map, size_t rows, size_t cols)
 
 void load_map(server_t *server)
 {
-    get_map_size(server->map_path, &server->map_rows, &server->map_cols);
-    server->map = allocate_map(server->map_rows, server->map_cols);
-    fill_map(server->map_path, server->map, server->map_rows,
-        server->map_cols);
+    get_map_size(server, &server->map_rows, &server->map_cols);
+    server->map = allocate_map(server);
+    fill_map(server->map_path, server->map, server);
 }
