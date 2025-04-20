b449ca6efbd4ef10be42be2493446e8e7aa0ce64
SantiagoPidji <santiago.pidcova@epitech.eu>
2025-04-20 10:25:12 +0200

    [FIX](server): Refine error handling in send_with_write function

--- server/send_function.c
+++ server/send_function.c
@@ -16,8 +16,10 @@ bool send_with_write(int fd, const void *buffer, size_t length)
 
     while (total_written < length) {
         bytes_written = write(fd, buf + total_written, length - total_written);
-        if (bytes_written == -1 || bytes_written == 0)
+        if (bytes_written == -1)
             return false;
+        if (bytes_written == 0)
+            break;
         total_written += bytes_written;
     }
     return true;
